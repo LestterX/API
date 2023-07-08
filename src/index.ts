70d49ce447125190c43fdba9066b70ff4bff05a6
LestterX <lgs.alves@hotmail.com>
2023-07-07 23:19:50 -0300

    Config: Padrão de Estilo com ESLINT

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import {server} from './server/server'
+import { server } from './server/server';
 
 server.listen('3333', () => {
-    console.log('Servidor Rodando Em: http://localhost:3333')
-})
+  console.log('Servidor Rodando Em: http://localhost:3333');
+});
