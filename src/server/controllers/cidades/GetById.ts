13e1e02f415cc9c1a3f8b17cc81aa3362e0d4304
LestterX <lgs.alves@hotmail.com>
2023-07-12 21:49:34 -0300

    DEL PUT GET (ById) - Cidades

---
+++ src/server/controllers/cidades/GetById.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import * as yup from 'yup';
+import { validation } from '../../shared/middlewares';
+import { StatusCodes } from 'http-status-codes';
+
+interface IParamsProps {
+  id?: number;
+}
+
+export const getByIdValidation = validation((getSchema) => ({
+  params: getSchema<IParamsProps>(yup.object({
+    id: yup.number().integer().required().moreThan(0),
+  })),
+}));
+
+export const getById = async (req: Request<IParamsProps, {}, {}, {}>, res: Response) => {
+  console.log(req.params);
+  
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Não imprementado');
+};
