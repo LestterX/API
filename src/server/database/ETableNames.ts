34d8a3625acfc2cd38c68c57e6a7ceb43929fde9
LestterX <lgs.alves@hotmail.com>
2023-07-28 23:07:36 -0300

    feat: migration cidade

---
+++ src/server/database/ETableNames.ts
@@ -0,0 +1,3 @@
+export enum ETableNames { //Cria chaves que equivale a tais valores
+  cidade = 'cidade',
+}
---
+++ src/server/database/migrations/0000_create_cidade.ts
@@ -0,0 +1,29 @@
+import { Knex } from 'knex';
+import { ETableNames } from '../ETableNames';
+
+
+export async function up(knex: Knex) {
+  return knex
+    .schema
+    .createTable('cidade', table => {
+      //bigInt autoIncrement || Primary Key || Diz que o ID será usado com frequência e otimizará
+      table.bigIncrements('id').primary().index();
+      table.string('nome', 150).index().notNullable();
+
+      table.comment('Table usada para armazenar cidades do sistema');
+    })
+    .then(() => {
+      // ETableNames.cidade --> Tem o retorno correto do nome 'cidade'
+      console.log(`# Created table ${ETableNames.cidade}`);
+    });
+}
+
+
+export async function down(knex: Knex) {
+  return knex
+    .schema
+    .dropTable(ETableNames.cidade)
+    .then(() => {
+      console.log(`# Dropped table ${ETableNames.cidade}`);
+    });
+}
